e3a14e7c58d4a393d4c171f3435d55b61c1469af
gmarvis <sgmarvis@gmail.com>
2025-01-26 20:12:45 +0100

    fixed deployment issues

--- app/model/media-file/index.ts
+++ app/model/media-file/index.ts
@@ -1,4 +1,4 @@
-import { Entity } from "../entity";
+import { Entity } from "~/model/entity";
 import { ConstructorType } from "../types";
 
 export enum FileType {
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     schema: "public",
   },
   dbCredentials: {
-    url: process.env.DATABASE_URL || ""
+    url: process.env.DATABASE_URL || ''
   }
 });
