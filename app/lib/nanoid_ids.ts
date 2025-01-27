cf3b7da538692e754fdd7245cd93e5d9688d67bc
Sam Gmarvis Njong <106551910+Gmarvis@users.noreply.github.com>
2025-01-27 15:04:18 +0100

    [Feature] Drizzle neon db setup
    * updated the domain model for media files and user

--- app/lib/nanoid_ids.ts
+++ app/lib/nanoid_ids.ts
@@ -3,6 +3,7 @@ const nanoid = customAlphabet("1234567890abcdefghijklmnopqrvuwxyz", 14);
 
 const prefixes = {
   user: "usr",
+  media: "med"
 };
 
 export function newPrefixedId(prefix: keyof typeof prefixes): string {
